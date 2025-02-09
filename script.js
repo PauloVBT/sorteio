cd7549384e10d6f21734a2e10a9b10626da81e76
PauloVBT <paulovbt1@gmail.com>
2025-02-09 16:34:00 -0300

    0.1

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function sortear(){
+async function sortear(){
     const totalResultados = Number(document.querySelector('#totalResultadosInput').value);
+
+    const menorValor = Number(document.querySelector('#menorValorInput').value);
 
     const maiorValor = Number(document.querySelector('#maiorValorInput').value);
 
-    const menorValor = Number(document.querySelector('#menorValorInput').value);
+    for(let j = 0; j < 20; j++){
+
+    const elementoHTMLResultsValues = document.querySelector('.results-values');
+
+    elementoHTMLResultsValues.innerHTML = '';
+
+    for(let i = 0; i < totalResultados; i++){
 
     const resultado = Math.floor(Math.random() * (maiorValor - menorValor + 1) + menorValor);
 
@@ -12,8 +20,15 @@ function sortear(){
     elementoHTMLDoResultado.classList.add('result-value');
 
     elementoHTMLDoResultado.innerText = resultado;
-
-    const elementoHTMLResultsValues = document.querySelector('.results-values');
     
     elementoHTMLResultsValues.append(elementoHTMLDoResultado);
+}
+ await wait(30)
+}
+}
+
+function wait(tempo){
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), tempo)
+    });
 }
